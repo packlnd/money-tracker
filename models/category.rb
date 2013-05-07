532898a3224b2afccdedcf6b4c2ace6b747a2b69
packlnd <patrikackland@gmail.com>
2013-05-07 21:31:56 +0200

    Lägg bara till kategorier en gång

--- models/category.rb
+++ models/category.rb
@@ -6,12 +6,14 @@ DB.create_table? :categories do
 	String :css, null: false
 end
 
-DB[:categories].insert(:name => "UTGIFT", :css => "label")
-DB[:categories].insert(:name => "INKOMST", :css => "label label-success")
-DB[:categories].insert(:name => "MAT OCH DRYCK", :css => "label label-warning")
-DB[:categories].insert(:name => "FRITID", :css => "label label-info")
-DB[:categories].insert(:name => "BANKOMAT", :css => "label label-inverse")
-DB[:categories].insert(:name => "TRANSPORT", :css => "label label-important")
+if DB[:categories].count == 0
+	DB[:categories].insert(:name => "UTGIFT", :css => "label")
+	DB[:categories].insert(:name => "INKOMST", :css => "label label-success")
+	DB[:categories].insert(:name => "MAT OCH DRYCK", :css => "label label-warning")
+	DB[:categories].insert(:name => "FRITID", :css => "label label-info")
+	DB[:categories].insert(:name => "BANKOMAT", :css => "label label-inverse")
+	DB[:categories].insert(:name => "TRANSPORT", :css => "label label-important")
+end
 
 class Category < Sequel::Model
 	one_to_many :transaction
