978d2e076295361b7aea43f5bc35c8980502029b
packlnd <patrikackland@gmail.com>
2013-05-24 20:40:37 +0200

    Städat upp lite

--- app/statistics.rb
+++ app/statistics.rb
@@ -1,6 +1,8 @@
 module App
   class Statistics < Sinatra::Application
 
+    require 'pry'
+
     before do
       env['warden'].authenticate!
     end
@@ -9,15 +11,8 @@ module App
       haml :statistics
     end
 
-#    post "/update" do
-#      categories = []
-#      params[:category].each do |cat|
-#        categories << Category[cat[0].to_i]
-#      end
-#      from = Time.new(params['date_from']) - 1
-#      to = Time.new(params['date_to']) + 3600*24
-#      create_statistics(categories, from, to)
-#      redirect '/statistics'
-#    end
+    post "/update" do
+      
+    end
   end
 end
