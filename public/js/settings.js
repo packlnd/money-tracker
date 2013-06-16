b6f3a4a57acf827792643825eb4169d4850dd0ae
packlnd <patrikackland@gmail.com>
2013-06-16 16:01:55 +0200

    Egengjord färgväljare till ny kategori

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,3 +1,11 @@
+var min_pos = 559, max_pos = 816, MOUSE_DOWN, $slider;
+
+$(document).ready(function() {
+  $(".slider#red").offset({left:min_pos});
+  $(".slider#green").offset({left:min_pos});
+  $(".slider#blue").offset({left:min_pos});
+});
+
 $(".save").click(function() {
   handle_category("add", "#setting_table");
 });
@@ -11,12 +19,43 @@ $(".delete").click(function() {
   format_request("settings/" + id + "/delete", "#setting_table");
 });
 
+$(".slider").mousedown(function(e) {
+  MOUSE_DOWN = true;
+  $slider = $(this);
+});
+
+function read_color(color) {
+  return parseInt($(".slider#" + color).offset().left) - min_pos;
+}
+
+function hex_color() {
+    var r = read_color("red"),
+      g = read_color("green"),
+      b = read_color("blue");
+  return r.toString("16") + g.toString("16") + b.toString("16");
+}
+
+$(document).mousemove(function(e) {
+  if (!MOUSE_DOWN || !$slider) {
+    return;
+  }
+  var curr_pos = e.pageX;
+  if (curr_pos > min_pos && curr_pos < max_pos) {
+    $slider.offset({left:curr_pos});
+  }
+});
+
+$(document).mouseup(function(e) {
+  $slider = undefined;
+  MOUSE_DOWN = false;
+  handle_category("format", "#templabel");
+});
+
 function handle_category(command, div) {
-  var color = $("#color").attr("value"),
-    name = $("#name").attr("value").toUpperCase();
+  var name = $("#name").attr("value").toUpperCase();
   if (name == "") {
     return;
   }
-  var url = "/settings/" + command + "/" + name + "/" + color;
+  var url = "/settings/" + command + "/" + name + "/" + hex_color();
   format_request(url, div);
 }
