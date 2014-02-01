bab5e894fada57f28c566976c43b7ed711a61e45
packlnd <patrikackland@gmail.com>
2014-02-01 16:55:31 +0100

    category list ser snyggare ut

--- models/user.rb
+++ models/user.rb
@@ -4,7 +4,6 @@ DB.create_table? :users do
   primary_key :id
   String :username, null: false, :unique=>true
   String :password, null: false
-  Boolean :set_all_categories, default: true
 end
 
 class User < Sequel::Model
